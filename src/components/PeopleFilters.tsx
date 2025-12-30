13dae04d2774cb42ea15799c9d518d5ed380433e
yahohulia <yaroslavhohulia@gmail.com>
2025-12-30 22:30:53 +0200

    added solution

--- src/components/PeopleFilters.tsx
+++ src/components/PeopleFilters.tsx
@@ -1,18 +1,106 @@
-export const PeopleFilters = () => {
+import React from 'react';
+import { NavLink, useSearchParams } from 'react-router-dom';
+import classNames from 'classnames';
+
+export const PeopleFilters: React.FC = () => {
+  const [searchParams, setSearchParams] = useSearchParams();
+  const query = searchParams.get('query') || '';
+  const centuriesArr = [16, 17, 18, 19, 20];
+  const centuries = searchParams.getAll('centuries') || [];
+  const currentSex = searchParams.get('sex');
+
+  const getLinkClass = (isActive: boolean, type: string | null) => {
+    return classNames({
+      'is-active':
+        type === null ? isActive && !currentSex : currentSex === type,
+    });
+  };
+
+  function handleQueryChange(event: React.ChangeEvent<HTMLInputElement>) {
+    const params = new URLSearchParams(searchParams);
+
+    if (!event.target.value.trim()) {
+      params.delete('query');
+    } else {
+      params.set('query', event.target.value);
+    }
+
+    setSearchParams(params);
+  }
+
+  function toggleCenturies(cent: string) {
+    const params = new URLSearchParams(searchParams);
+    const newCenturies = centuries.includes(cent)
+      ? centuries.filter(century => century !== cent)
+      : [...centuries, cent];
+
+    params.delete('centuries');
+
+    newCenturies.forEach(century => params.append('centuries', century));
+
+    setSearchParams(params);
+  }
+
+  function toggleAllCenturies() {
+    const params = new URLSearchParams(searchParams);
+
+    params.delete('centuries');
+
+    setSearchParams(params);
+  }
+
+  const getSearchWith = (sexValue: string | null) => {
+    const newParams = new URLSearchParams(searchParams);
+
+    if (sexValue === null) {
+      newParams.delete('sex');
+    } else {
+      newParams.set('sex', sexValue);
+    }
+
+    return newParams.toString();
+  };
+
+  const handleResetAllFillters = () => {
+    const newParams = new URLSearchParams();
+
+    const currentSort = searchParams.get('sort');
+    const currentOrder = searchParams.get('order');
+
+    if (currentSort) {
+      newParams.set('sort', currentSort);
+    }
+
+    if (currentOrder) {
+      newParams.set('order', currentOrder);
+    }
+
+    setSearchParams(newParams);
+  };
+
   return (
     <nav className="panel">
       <p className="panel-heading">Filters</p>
 
       <p className="panel-tabs" data-cy="SexFilter">
-        <a className="is-active" href="#/people">
+        <NavLink
+          className={({ isActive }) => getLinkClass(isActive, null)}
+          to={{ search: getSearchWith(null) }}
+        >
           All
-        </a>
-        <a className="" href="#/people?sex=m">
+        </NavLink>
+        <NavLink
+          className={getLinkClass(false, 'm')}
+          to={{ search: getSearchWith('m') }}
+        >
           Male
-        </a>
-        <a className="" href="#/people?sex=f">
+        </NavLink>
+        <NavLink
+          className={getLinkClass(false, 'f')}
+          to={{ search: getSearchWith('f') }}
+        >
           Female
-        </a>
+        </NavLink>
       </p>
 
       <div className="panel-block">
@@ -22,6 +110,8 @@ export const PeopleFilters = () => {
             type="search"
             className="input"
             placeholder="Search"
+            value={query}
+            onChange={handleQueryChange}
           />
 
           <span className="icon is-left">
@@ -33,52 +123,29 @@ export const PeopleFilters = () => {
       <div className="panel-block">
         <div className="level is-flex-grow-1 is-mobile" data-cy="CenturyFilter">
           <div className="level-left">
-            <a
-              data-cy="century"
-              className="button mr-1"
-              href="#/people?centuries=16"
-            >
-              16
-            </a>
-
-            <a
-              data-cy="century"
-              className="button mr-1 is-info"
-              href="#/people?centuries=17"
-            >
-              17
-            </a>
-
-            <a
-              data-cy="century"
-              className="button mr-1 is-info"
-              href="#/people?centuries=18"
-            >
-              18
-            </a>
-
-            <a
-              data-cy="century"
-              className="button mr-1 is-info"
-              href="#/people?centuries=19"
-            >
-              19
-            </a>
-
-            <a
-              data-cy="century"
-              className="button mr-1"
-              href="#/people?centuries=20"
-            >
-              20
-            </a>
+            {centuriesArr.map(century => {
+              return (
+                <button
+                  key={century}
+                  data-cy="century"
+                  className={classNames('button mr-1', {
+                    'is-info': centuries.includes(century.toString()),
+                  })}
+                  onClick={() => toggleCenturies(century.toString())}
+                >
+                  {century}
+                </button>
+              );
+            })}
           </div>
 
           <div className="level-right ml-4">
             <a
               data-cy="centuryALL"
-              className="button is-success is-outlined"
-              href="#/people"
+              className={classNames('button is-success', {
+                'is-outlined': centuries.length !== 0,
+              })}
+              onClick={() => toggleAllCenturies()}
             >
               All
             </a>
@@ -87,9 +154,12 @@ export const PeopleFilters = () => {
       </div>
 
       <div className="panel-block">
-        <a className="button is-link is-outlined is-fullwidth" href="#/people">
+        <button
+          className="button is-link is-outlined is-fullwidth"
+          onClick={handleResetAllFillters}
+        >
           Reset all filters
-        </a>
+        </button>
       </div>
     </nav>
   );
--- src/components/PeoplePage.tsx
+++ src/components/PeoplePage.tsx
@@ -1,33 +1,208 @@
-import { PeopleFilters } from './PeopleFilters';
+import { useEffect, useState } from 'react';
+import { Person } from '../types';
 import { Loader } from './Loader';
+import { getPeople } from '../api';
 import { PeopleTable } from './PeopleTable';
+import { PeopleFilters } from './PeopleFilters';
+import { useSearchParams } from 'react-router-dom';
+import classNames from 'classnames';
 
 export const PeoplePage = () => {
+  const [searchParams, setSearchParams] = useSearchParams();
+  const [hasError, setHasError] = useState(false);
+  const [isLoading, setIsLoading] = useState(false);
+  const [people, setPeople] = useState<Person[]>([]);
+
+  const sortField = searchParams.get('sort');
+  const sortOrder = searchParams.get('order');
+  const query = searchParams.get('query');
+  const sex = searchParams.get('sex');
+  const centuries = searchParams.getAll('centuries');
+
+  useEffect(() => {
+    setHasError(false);
+    setIsLoading(true);
+
+    getPeople()
+      .then(setPeople)
+      .catch(() => setHasError(true))
+      .finally(() => setIsLoading(false));
+  }, []);
+
+  function handleSortClick(field: string) {
+    const params = new URLSearchParams(searchParams);
+    const sort = searchParams.get('sort');
+
+    if (sort !== field) {
+      params.set('sort', field);
+      params.delete('order');
+    } else {
+      if (!params.has('order')) {
+        params.set('order', 'desc');
+      } else {
+        params.delete('order');
+        params.delete('sort');
+      }
+    }
+
+    setSearchParams(params);
+  }
+
+  const getSortIcon = (field: string) => {
+    const currentSort = searchParams.get('sort');
+    const currentOrder = searchParams.get('order');
+
+    return classNames('fas', {
+      'fa-sort': currentSort !== field,
+      'fa-sort-up': currentSort === field && !currentOrder,
+      'fa-sort-down': currentSort === field && currentOrder === 'desc',
+    });
+  };
+
+  const getVisiblePeople = () => {
+    let result = [...people];
+
+    if (query) {
+      const normalizedQuery = query.toLowerCase().trim();
+
+      result = result.filter(
+        person =>
+          person.name.toLocaleLowerCase().includes(normalizedQuery) ||
+          person.motherName?.toLocaleLowerCase().includes(normalizedQuery) ||
+          person.fatherName?.toLocaleLowerCase().includes(normalizedQuery),
+      );
+    }
+
+    if (sex) {
+      result = result.filter(person => person.sex === sex);
+    }
+
+    if (centuries.length > 0) {
+      result = result.filter(person => {
+        const centuryBorn = Math.ceil(person.born / 100).toString();
+
+        return centuries.includes(centuryBorn);
+      });
+    }
+
+    if (sortField) {
+      result.sort((person1, person2) => {
+        const value1 = person1[sortField as keyof Person];
+        const value2 = person2[sortField as keyof Person];
+
+        if (typeof value1 === 'string' && typeof value2 === 'string') {
+          return value1.localeCompare(value2);
+        }
+
+        if (typeof value1 === 'number' && typeof value2 === 'number') {
+          return value1 - value2;
+        }
+
+        return 0;
+      });
+
+      if (sortOrder === 'desc') {
+        result.reverse();
+      }
+    }
+
+    return result;
+  };
+
+  const visiblePeople = getVisiblePeople();
+
   return (
-    <>
+    <div className="container">
       <h1 className="title">People Page</h1>
 
       <div className="block">
-        <div className="columns is-desktop is-flex-direction-row-reverse">
-          <div className="column is-7-tablet is-narrow-desktop">
-            <PeopleFilters />
-          </div>
-
-          <div className="column">
+        {isLoading ? (
+          <Loader />
+        ) : hasError ? (
+          <p data-cy="peopleLoadingError" className="has-text-danger">
+            Something went wrong
+          </p>
+        ) : people.length === 0 ? (
+          <p data-cy="noPeopleMessage">There are no people on the server</p>
+        ) : (
+          <div className="columns is-desktop is-flex-direction-row-reverse">
+            <div className="column is-7-tablet is-narrow-desktop">
+              <PeopleFilters />
+            </div>
             <div className="box table-container">
-              <Loader />
+              <table
+                data-cy="peopleTable"
+                className="table is-striped is-hoverable is-narrow is-fullwidth"
+              >
+                <thead>
+                  <tr>
+                    <th>
+                      <span
+                        className="is-flex is-flex-wrap-nowrap"
+                        onClick={() => handleSortClick('name')}
+                      >
+                        Name
+                        <a>
+                          <span className="icon">
+                            <i className={getSortIcon('name')} />
+                          </span>
+                        </a>
+                      </span>
+                    </th>
 
-              <p data-cy="peopleLoadingError">Something went wrong</p>
+                    <th>
+                      <span
+                        className="is-flex is-flex-wrap-nowrap"
+                        onClick={() => handleSortClick('sex')}
+                      >
+                        Sex
+                        <a>
+                          <span className="icon">
+                            <i className={getSortIcon('sex')} />
+                          </span>
+                        </a>
+                      </span>
+                    </th>
 
-              <p data-cy="noPeopleMessage">There are no people on the server</p>
+                    <th>
+                      <span
+                        className="is-flex is-flex-wrap-nowrap"
+                        onClick={() => handleSortClick('born')}
+                      >
+                        Born
+                        <a>
+                          <span className="icon">
+                            <i className={getSortIcon('born')} />
+                          </span>
+                        </a>
+                      </span>
+                    </th>
 
-              <p>There are no people matching the current search criteria</p>
+                    <th>
+                      <span
+                        className="is-flex is-flex-wrap-nowrap"
+                        onClick={() => handleSortClick('died')}
+                      >
+                        Died
+                        <a>
+                          <span className="icon">
+                            <i className={getSortIcon('died')} />
+                          </span>
+                        </a>
+                      </span>
+                    </th>
 
-              <PeopleTable />
+                    <th>Mother</th>
+                    <th>Father</th>
+                  </tr>
+                </thead>
+
+                {visiblePeople && <PeopleTable people={visiblePeople} />}
+              </table>
             </div>
           </div>
-        </div>
+        )}
       </div>
-    </>
+    </div>
   );
 };
